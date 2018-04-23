f6ee8b396caf29388431745457d045e1daf883c6
Shakeel Samsudeen <shakeelsamsudeen15@gmail.com>
2018-04-23 12:19:04 -0500

    Add files via upload

---
+++ src/QuickEstimates.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class QuickEstimates {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		for(int i = 0; i < n; i++) {
+			String price = in.next();
+			int estimate = price.length();
+			System.out.println(estimate);
+		}
+	}
+
+}
