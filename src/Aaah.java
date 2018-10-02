67440ce725bedc66423c51cd090badf2db87c970
Shakeel <shakeelsamsudeen15@gmail.com>
2018-10-01 20:05:26 -0500

    more formatting

--- src/Aaah.java
+++ src/Aaah.java
@@ -1,7 +1,13 @@
+/**
+ * @author: shakeelsamsu
+ * https://github.com/shakeelsamsu
+ * Apr 25 18
+ */
+
 import java.util.*;
 import java.io.*;
 
-public class Aaah {
+public class aaah {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
