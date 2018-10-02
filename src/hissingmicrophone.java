ff17527ef9dae0dfd5643f46ee46a2f5a666ab37
Shakeel Samsudeen <shakeelsamsudeen15@gmail.com>
2018-10-02 11:05:48 -0500

    renaming to match convention

--- src/hissingmicrophone.java
+++ src/hissingmicrophone.java
@@ -7,7 +7,7 @@
 import java.io.*;
 import java.util.*;
 
-public class HissingMicrophone {
+public class hissingmicrophone {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
