ef1001b946a85cf21f5e0c63782354cc2dd8d762
shakeelsamsu <shakeelsamsudeen15@gmail.com>
2018-10-06 16:39:26 -0500

    sumbitted dst and justaminute

---
+++ src/dst.java
@@ -0,0 +1,60 @@
+/**
+ * @author: shakeelsamsu
+ * https://github.com/shakeelsamsu
+ * Oct 6 19 4:07:29 PM
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class dst {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(in.readLine());
+            String direction = st.nextToken();
+            boolean forwards = direction.equals("F");
+            int change = Integer.parseInt(st.nextToken());
+            int hour = Integer.parseInt(st.nextToken());
+            int minute = Integer.parseInt(st.nextToken());
+            int changeHour = change / 60;
+            int changeMinute = change % 60;
+            if(forwards) {
+                if(minute + changeMinute >= 60) {
+                    if(hour == 23) hour = 0;
+                    else hour++;
+                    minute -= 60 - changeMinute;
+                }
+                else {
+                    minute += changeMinute;
+                }
+                if(hour + changeHour >= 24) {
+                    hour = (hour + changeHour) % 24;
+                    changeHour -= 1;
+                }
+                else hour += changeHour;
+            }
+            else {
+                if(minute - changeMinute < 0) {
+                    if(hour <= 0) hour = 23;
+                    else hour--;
+                    minute += 60 - changeMinute;
+                }
+                else {
+                    minute -= changeMinute;
+                }
+                if(hour - changeHour < 0) {
+                    hour = 23;
+                    changeHour -= 1;
+                }
+                else hour -= changeHour;
+            }
+            out.println(hour + " " + minute);    
+        }
+        out.close();
+    }
+}
---
+++ src/justaminute.java
@@ -0,0 +1,31 @@
+/**
+ * @author: shakeelsamsu
+ * https://github.com/shakeelsamsu
+ * Oct 6 2018 2:24:32 PM
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class justaminute {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        double sum = 0;
+        int mins = 0;
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(in.readLine());
+            int minutes = Integer.parseInt(st.nextToken());
+            double seconds = (double) Integer.parseInt(st.nextToken());
+            sum += (seconds / (minutes * 60)) * minutes;
+            mins += minutes;
+        }
+        double average = sum / mins;
+        if(average <= 1) out.println("measurement error");
+        else out.printf("%.9f", average);
+        out.close();
+    }
+}
