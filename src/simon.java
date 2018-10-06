e2c8ceeea6bf20a827954d93237d93b05ab70756
shakeelsamsu <shakeelsamsudeen15@gmail.com>
2018-10-06 09:05:30 -0500

    sumbitted simon

---
+++ src/simon.java
@@ -0,0 +1,31 @@
+/**
+ * @author: shakeelsamsu
+ * https://github.com/shakeelsamsu
+ * Oct 6 18 8:54:26 AM
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class simon {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < n; i++) {
+            String[] input = in.readLine().split(" ");
+            ArrayList<String> phrase = new ArrayList<String>();
+            for(String s : input) phrase.add(s);
+            if(phrase.contains("simon") && phrase.contains("says")) {
+                for(int k = 2;  k < phrase.size(); k++) {
+                    System.out.print(phrase.get(k) + " ");
+                }
+                System.out.println();
+            }
+            else {
+                System.out.println();
+            }
+        }
+    }
+}
