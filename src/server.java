b24f60e37af4563127e1af553767d0c3ef0c0f78
shakeelsamsu <shakeelsamsudeen15@gmail.com>
2018-10-07 23:05:08 -0500

    submitted server

---
+++ src/server.java
@@ -0,0 +1,23 @@
+/**
+ * @author: shakeelsamsu
+ * https://github.com/shakeelsamsu
+ * Oct 7 18 11:00:04 PM
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class server {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int total = in.nextInt();
+        int sum = 0;
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            sum += in.nextInt();
+            if(sum <= total) count++;
+        }
+        System.out.println(count);
+    }
+}
