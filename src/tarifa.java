9aa2447f55acea997c357e27ef234a7003937220
shakeelsamsu <shakeelsamsudeen15@gmail.com>
2018-10-10 21:26:06 -0500

    submitted tarifa

---
+++ src/tarifa.java
@@ -0,0 +1,20 @@
+/**
+ * @author: shakeelsamsu
+ * https://github.com/shakeelsamsu
+ * Oct 10 18 9:25:24 PM
+ */
+import java.io.*;
+import java.util.*;
+
+public class tarifa {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        int n = in.nextInt();
+        int available = x;
+        for(int i = 0; i < n; i++) {
+            available += (x - in.nextInt());
+        }
+        System.out.println(available);
+    }
+}
