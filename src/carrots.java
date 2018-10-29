a8e2a06b5108d94dae6b5b7186caca33977205f9
shakeelsamsu <shakeelsamsudeen15@gmail.com>
2018-10-28 21:54:50 -0500

    solved carrots

---
+++ src/carrots.java
@@ -0,0 +1,15 @@
+/**
+ * @author: shakeelsamsu
+ * https://github.com/shakeelsamsu
+ * Oct 28 18 21:52:17
+ */
+
+import java.util.Scanner;
+
+public class carrots {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        in.nextInt();
+        System.out.println(in.nextInt());
+    }
+}
