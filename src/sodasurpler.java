28f11a345d7d53dc63031b39f376a99878eb3783
Shakeel <shakeelsamsudeen15@gmail.com>
2019-01-03 12:00:54 -0600

    solved sodasurpler

---
+++ src/sodasurpler.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class sodasurpler {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+
+        int bottles = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+        int req = Integer.parseInt(st.nextToken());
+        int sodas = 0;
+        while(bottles - req >= 0) {
+            bottles -= req;
+            bottles++;
+            sodas++;
+        }
+        out.println(sodas);
+        out.close();
+    }
+}
