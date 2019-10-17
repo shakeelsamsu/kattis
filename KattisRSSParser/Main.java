6d3a1e3e5e7a548aeda815e5a5bc838a6e2a52d6
JB Ladera <32505118+MiniDomo@users.noreply.github.com>
2019-10-17 17:11:21 -0500

    switched to xml parser

--- KattisRSSParser/Main.java
+++ KattisRSSParser/Main.java
@@ -3,7 +3,8 @@ import java.util.*;
 import java.net.*;
 
 public class Main {
-    static final String USER_AGENT = ""; //replace with your own user-agent String
+    private static final String USER_AGENT = ""; // replace with your own user-agent String
+
     public static void main(String[] args) throws Exception {
 
         URL url = new URL("https://open.kattis.com/rss/new-problems");
@@ -11,11 +12,8 @@ public class Main {
         URLConnection connection = url.openConnection();
         connection.addRequestProperty("User-Agent", USER_AGENT);
         InputStream input = connection.getInputStream();
-        BufferedReader in = new BufferedReader(new InputStreamReader(input));
-
-        Parser parser = new Parser(in);
-        ArrayList<Problem> problems = new ArrayList<Problem>();
-        problems = parser.parse();
+        Parser parser = new Parser(input);
+        ArrayList<Problem> problems = parser.parse();
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("Problems.md")));
         Tabler.write(out, problems);
         out.close();
