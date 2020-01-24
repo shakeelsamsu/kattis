7d30987fc94f289e64fa5f33ffb1d0c38dcd5442
Shakeel <shakeelsamsudeen15@gmail.com>
2020-01-23 20:42:13 -0600

    updated htoo solution

--- src/htoo.java
+++ src/htoo.java
@@ -1,70 +1,50 @@
 import java.io.*;
 import java.util.*;
+import java.math.*;
+import java.util.regex.Pattern;
 
 public class htoo {
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        StringTokenizer st = new StringTokenizer(in.readLine());
 
-        int[] elements = new int[26];
-        Arrays.fill(elements, Integer.MAX_VALUE);
-        String[] input = in.readLine().split("");
-        int i = Arrays.asList(input).indexOf(" ") + 1;
-        String temp = "";
-        for(; i < input.length; i++) {
-            temp += input[i];
+        String molecule = st.nextToken();
+        int n = Integer.parseInt(st.nextToken());
+        String result = in.readLine();
+
+        HashMap<String, Integer> pool = new HashMap<String, Integer>();
+        int i = 0;
+        while(i < molecule.length()) {
+            String key = molecule.substring(i, i++ + 1);
+            String num = "";
+            while(i < molecule.length() && molecule.substring(i, i + 1).matches("[0-9]"))
+                num += molecule.substring(i, i++ + 1);
+            int res = ((num.equals("") ? 1 : Integer.parseInt(num)) * n);
+            pool.put(key, pool.get(key) != null ? pool.get(key) + res : res);
         }
-        int multiplier = Integer.parseInt(temp);
+
+        HashMap<String, Integer> required = new HashMap<String, Integer>();
         i = 0;
-        while(!input[i].equals(" ")) {
-            char element = input[i].charAt(0);
-            i++;
-            temp = "";
-            while(input[i].matches("\\d+")) {
-                temp += input[i];
-                i++;
-            } 
-            if(temp.equals("")) temp = "1";
-            if(elements[element - 'A'] == Integer.MAX_VALUE) elements[element - 'A'] = Integer.parseInt(temp) * multiplier;
-            else elements[element - 'A'] = elements[element - 'A'] + Integer.parseInt(temp) * multiplier;
+        while(i < result.length()) {
+            String key = result.substring(i, i++ + 1);
+            String num = "";
+            while(i < result.length() && result.substring(i, i + 1).matches("[0-9]"))
+                num += result.substring(i, i++ + 1);
+            int res = ((num.equals("") ? 1 : Integer.parseInt(num)));
+            required.put(key, required.get(key) != null ? required.get(key) + res : res);
         }
-        // out.println(Arrays.toString(elements));
-        String[] product = in.readLine().split("");
-        i = 0;
-        boolean impossible = false;
+
         int min = Integer.MAX_VALUE;
-        int[] required = new int[26];
-        Arrays.fill(required, Integer.MAX_VALUE);
-        for(; !impossible && i < product.length; i++) {
-            char element = product[i].charAt(0);
-            temp = "";
-            while(i + 1 < product.length && product[i + 1].matches("\\d+")) {
-                i++;
-                temp += product[i];
-            } 
-            if(temp.equals("")) temp = "1";
-            int req = Integer.parseInt(temp);
-            int index = element - 'A';
-            if(elements[index] == Integer.MAX_VALUE) {
-                impossible = true;
+        for(Map.Entry<String, Integer> element : required.entrySet()) {
+            String key = element.getKey();
+            if(pool.get(key) == null) {
+                min = 0;
+                break;
             }
-            else {
-                if(required[index] == Integer.MAX_VALUE) required[index] = req;
-                else required[index] = required[index] + req;
-            }
-            // out.println(i);
+            min = Math.min(pool.get(key) / required.get(key), min);
         }
-        for(i = 0; !impossible && i < 26; i++) {
-            if(elements[i] != Integer.MAX_VALUE && required[i] != Integer.MAX_VALUE) {
-                elements[i] = elements[i] / required[i];
-                if(elements[i] < min) min = elements[i];
-            }
-        }
-        // out.println(Arrays.toString(required));
-        // out.println(Arrays.toString(elements));
-        // out.println(impossible);
-        if(impossible) out.println(0);
-        else out.println(min);
+        out.println(min);
         out.close();
     }
 }
