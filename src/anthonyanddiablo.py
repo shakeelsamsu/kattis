9cf48c61d454488e6a7c116b5ec2d0f41ca250d5
Shakeel <shakeelsamsudeen15@gmail.com>
2020-06-30 14:11:35 -0500

    shorten solution

--- src/anthonyanddiablo.py
+++ src/anthonyanddiablo.py
@@ -1,4 +1,3 @@
 import math
 a, n = [float(x) for x in raw_input().split()]
-r = math.sqrt(a/math.pi)
-print('Diablo is Happy!' if 2 * math.pi * r <= n else 'Need more materials!')
+print('Diablo is Happy!' if 2 * math.pi * math.sqrt(a/math.pi) <= n else 'Need more materials!')
